ce7a5b9a7518c1064ea78bdc53d8a2e77ac0fa51
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-03-13 12:55:18 +0100

    Added batch file which work wonders

---
+++ volpe.bat
@@ -0,0 +1,3 @@
+@echo off
+cd /D "%~dp0"
+python volpe/__main__.py %*
--- volpe/__main__.py
+++ volpe/__main__.py
@@ -16,51 +16,28 @@ from docopt import docopt
 
 
 def install():
-    needs_restart = False
+    # Look up user path in registry.
+    import winreg
+    reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Environment')
+    path = winreg.QueryValueEx(reg_key, 'Path')[0]
+
     path_to_volpe = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
-    # print(path_to_volpe)
-    # print("PATH:\n", os.environ["PATH"])
-    # print("PATHETX:\n", os.environ["PATHEXT"])
-
-    if path_to_volpe in os.environ["PATH"]:
+    if path_to_volpe in path:
         print("Volpe already on PATH")
+        
+        if not path_to_volpe in os.environ["PATH"]:
+            print("Please restart this console for changes to take effect.")
 
     else:
-        # Look up user path in registry.
-        import winreg
-        reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r'Environment')
-        path = winreg.QueryValueEx(reg_key, 'Path')[0]
-
         # Add Volpe path to user path.
         print(f"adding {path_to_volpe} to user PATH")
         path_with_volpe = path + os.pathsep + path_to_volpe
         print("setting local user PATH")
         os.system(f'SETX Path "{path_with_volpe}"')
-        needs_restart = True
-
-    print("=====")
-
-    if ".PY" in os.environ["PATHEXT"].upper():
-        print(".PY already in PATHEXT")
-
-    else:
-        # Add .PY to PATHEXT
-        print("adding .PY to PATHEXT")
-        os.environ["PATHEXT"] += os.pathsep + ".PY"
-        pathext_var = os.environ["PATHEXT"]
-
-        print("setting PATHETX")
-        os.system(f'SETX PATHEXT "{pathext_var}" /M')
-        needs_restart = True
-    
-    print("=====")
-
-    if needs_restart:
-        print("In anything failed, try running this in a console as administrator.")
+        
+        print("=====")
         print("Please restart this console for changes to take effect.")
-    else:
-        print("Everything seems good to go!")
 
 
 def compile_and_run(file_path, verbose=False):
