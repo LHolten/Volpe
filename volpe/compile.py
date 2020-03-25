18af8e470b676204f8fc6041dd7476f1dba4eb9e
Viliam Vadocz <viliam.vadocz@gmail.com>
2020-03-25 17:44:10 +0100

    Add basic support for iterators and lists

--- volpe/compile.py
+++ volpe/compile.py
@@ -4,7 +4,16 @@ import llvmlite.binding as llvm
 
 
 # All these initializations are required for code generation!
-from volpe_types import int1, int32, flt32, flt64, VolpeTuple, VolpeClosure
+from volpe_types import (
+    int1, 
+    int32, 
+    flt32, 
+    flt64, 
+    VolpeTuple, 
+    VolpeClosure,
+    VolpeIterator,
+    VolpeList
+)
 
 llvm.initialize()
 llvm.initialize_native_target()
@@ -38,22 +47,9 @@ def compile_and_run(llvm_ir, result_type):
 
 
 def determine_c_type(volpe_type, depth=0):
-    # Interpret result type.
-    if isinstance(volpe_type, VolpeTuple):
-        elems = volpe_type.elements
-        class CTuple(Structure):
-            _fields_ = [(f"elem{i}", determine_c_type(elem, depth+1)) for i, elem in enumerate(elems)]
-            def __repr__(self):
-                return "[" + ", ".join([str(getattr(self, tup[0])) for tup in self._fields_]) + "]"
-        
-        return POINTER(CTuple) if depth == 0 else CTuple
-    if isinstance(volpe_type, VolpeClosure):
-        class CFunc(Structure):
-            _fields_ = [("func", POINTER(None)), ("c_func", POINTER(None)), ("f_func", POINTER(None)), ("env", POINTER(None))]
-            def __repr__(self):
-                return "*function*"
-        return CFunc
-    elif volpe_type == int1:
+    """Interpret the volpe type and return a corresponding C type."""
+    # Simple types:
+    if volpe_type == int1:
         return c_bool
     elif volpe_type == int32:
         return c_int32
@@ -61,5 +57,42 @@ def determine_c_type(volpe_type, depth=0):
         return c_float
     elif volpe_type == flt64:
         return c_double
-    else:
-        return None
+    
+    # Aggregate types:
+    elif isinstance(volpe_type, VolpeTuple):
+        print(volpe_type._to_string())
+        elems = volpe_type.elements
+        class CTuple(Structure):
+            _fields_ = [(f"elem{i}", determine_c_type(elem, depth+1)) for i, elem in enumerate(elems)]
+            def __repr__(self):
+                return "[" + ", ".join([str(getattr(self, tup[0])) for tup in self._fields_]) + "]"
+        return POINTER(CTuple) if depth == 0 else CTuple
+
+    elif isinstance(volpe_type, VolpeClosure):
+        print(volpe_type._to_string())
+        class CFunc(Structure):
+            _fields_ = [("func", POINTER(None)), ("c_func", POINTER(None)), ("f_func", POINTER(None)), ("env", POINTER(None))]
+            def __repr__(self):
+                return "*function*"
+        return CFunc
+
+    elif isinstance(volpe_type, VolpeIterator):
+        print(volpe_type._to_string())
+        class CFunc(Structure):
+            _fields_ = [("func", POINTER(None)), ("c_func", POINTER(None)), ("f_func", POINTER(None)), ("env", POINTER(None))]
+        class CIterator(Structure):
+            _fields_ = [("func", CFunc), ("num", c_int32)]
+            def __repr__(self):
+                return "*iterator*"
+        return CIterator
+
+    elif isinstance(volpe_type, VolpeList):
+        print(volpe_type._to_string())
+        class CList(Structure):
+            _fields_ = [("elem_type", POINTER(None)), ("num", c_int32)]
+            def __repr__(self):
+                return "*list*"
+        return CList
+        
+    # Unknown type
+    else: return None
