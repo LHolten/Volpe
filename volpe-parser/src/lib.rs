969f4b6c721244ba96f46e7e0cdf13ddc61fa1d3
Hytak <lhc.holten@gmail.com>
2020-12-11 16:48:44 +0100

    update grammar

--- volpe-parser/src/lib.rs
+++ volpe-parser/src/lib.rs
@@ -5,13 +5,14 @@ lalrpop_mod!(pub volpe);
 
 #[cfg(test)]
 mod tests {
-    use crate::volpe::BlockParser;
+    use crate::volpe::ExprParser;
 
     #[test]
     fn functions() {
-        assert!(BlockParser::new().parse("hello world").is_ok());
-        assert!(BlockParser::new()
+        assert!(ExprParser::new().parse("hello world").is_ok());
+        assert!(ExprParser::new()
             .parse("[1, 2, 3; 4, 5, 6] 10 (cool thing)")
             .is_ok());
+        assert!(ExprParser::new().parse("(a = 1; b = 2; add a b)").is_ok());
     }
 }
