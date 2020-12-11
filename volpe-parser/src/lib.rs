38776e2904001a919706c8e65cb75e279f07f428
Hytak <lhc.holten@gmail.com>
2020-12-11 15:13:07 +0100

    set up lalrpop

--- volpe-parser/src/lib.rs
+++ volpe-parser/src/lib.rs
@@ -1,7 +1,17 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub volpe);
+
 #[cfg(test)]
 mod tests {
+    use crate::volpe::TermParser;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn calculator1() {
+        assert!(TermParser::new().parse("22").is_ok());
+        assert!(TermParser::new().parse("(22)").is_ok());
+        assert!(TermParser::new().parse("((((22))))").is_ok());
+        assert!(TermParser::new().parse("((22)").is_err());
     }
 }
