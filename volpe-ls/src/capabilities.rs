3b1f8999d80b587b3ab31590b92acf7dda8a6911
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-11 21:31:55 +0100

    Add capabilities file

---
+++ volpe-ls/src/capabilities.rs
@@ -0,0 +1,8 @@
+use lsp_types::{ServerCapabilities, TextDocumentSyncCapability, TextDocumentSyncKind};
+
+pub fn server_capabilities() -> ServerCapabilities {
+    ServerCapabilities {
+        text_document_sync: Some(TextDocumentSyncCapability::Kind(TextDocumentSyncKind::Incremental)),
+        ..Default::default()
+    }
+}
--- volpe-ls/src/main.rs
+++ volpe-ls/src/main.rs
@@ -1,15 +1,16 @@
+use capabilities::server_capabilities;
 use lsp_server::Connection;
-use lsp_types::ServerCapabilities;
 use serde_json;
 
+mod capabilities;
 mod server;
 use server::Server;
 
 fn main() {
-    let (connection, io_threads) = Connection::stdio();
+    let (connection, _io_threads) = Connection::stdio();
 
-    let server_capabilities = serde_json::to_value(&ServerCapabilities::default()).unwrap();
-    let init_params = connection.initialize(server_capabilities).unwrap();
+    let server_capabilities = serde_json::to_value(capabilities::server_capabilities()).unwrap();
+    let _init_params = connection.initialize(server_capabilities).unwrap();
 
     Server::new(connection).run();
 }
