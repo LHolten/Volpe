570ba1ee11e33fb8bce2e1bcca1e3ae78b285164
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-02-19 22:03:40 +0100

    Use utf-8 offset encoding

--- volpe-ls/src/main.rs
+++ volpe-ls/src/main.rs
@@ -1,4 +1,5 @@
 use lsp_server::Connection;
+use lsp_types;
 use serde_json;
 
 mod capabilities;
@@ -10,8 +11,17 @@ use server::Server;
 fn main() {
     let (connection, _io_threads) = Connection::stdio();
 
-    let server_capabilities = serde_json::to_value(capabilities::server_capabilities()).unwrap();
-    let _init_params = connection.initialize(server_capabilities).unwrap();
+    let (initialize_id, initialize_params) = connection.initialize_start().unwrap();
+    let _init_params: lsp_types::InitializeParams = serde_json::from_value(initialize_params).unwrap();
 
+    let initialize_result = lsp_types::InitializeResult {
+        capabilities: capabilities::server_capabilities(),
+        server_info: None,
+        offset_encoding: Some("utf-8".to_string()), // we ONLY support utf-8
+    };
+    let initialize_result = serde_json::to_value(initialize_result).unwrap();
+
+    connection.initialize_finish(initialize_id, initialize_result).unwrap();
+    
     Server::new(connection).run();
 }
