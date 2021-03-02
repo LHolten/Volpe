6170ba5317a372275179e829000b15141a5e4e07
Hytak <lhc.holten@gmail.com>
2021-03-02 22:19:16 +0100

    Move packrat parser to replace lalrpop parser

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,4 @@
-use offset::Offset;
-
-use crate::packrat::Syntax;
-
-extern crate logos;
-extern crate typed_arena;
 extern crate volpe_parser;
 extern crate z3;
 
-mod lexer;
-mod offset;
-mod packrat;
-mod parser;
-
-fn main() {
-    let mut pos = Syntax::default();
-    pos = pos.parse(
-        "b || c 
-* 2",
-        Offset::char(0),
-        Offset::char(0),
-    );
-    dbg!(pos.text());
-    dbg!(&pos);
-    pos = pos.parse("", Offset::line(), Offset::char(1));
-    dbg!(pos.text());
-    dbg!(&pos);
-}
+fn main() {}
--- volpe-parser/src/lib.rs
+++ volpe-parser/src/lib.rs
@@ -1,67 +1,8 @@
+extern crate logos;
+extern crate typed_arena;
+
 pub mod ast;
-#[allow(clippy::all)]
-pub mod parser;
-
-#[cfg(test)]
-mod tests {
-    use crate::parser::{ExprParser, ObjectParser};
-
-    macro_rules! test_expr {
-        ($s:literal) => {
-            assert!(ExprParser::new().parse($s).is_ok())
-        };
-    }
-
-    #[test]
-    fn functions() {
-        test_expr!("hello world");
-        test_expr!("[1, 2, 3; 4, 5, 6] 10 (cool thing)");
-        test_expr!("(a = 1; b = 2; add a b)");
-        test_expr!(
-            "my_object = {
-                alpha : something,
-                beta : 3404,
-            }; my_object"
-        );
-        test_expr!("a.b.(add a b) 10 20");
-        test_expr!("{1, 2, 3}");
-        test_expr!("{1}");
-        test_expr!("{}");
-        test_expr!("1 > 2 => {}");
-        test_expr!("1 /* /* wow */ cool */ > 2 // hello");
-    }
-
-    #[test]
-    fn complicated_ast() {
-        assert!(dbg!(ObjectParser::new().parse(
-            "// for loop implementation
-            for: iter.func.{
-                exec: iter {
-                    some: val.(
-                        func val;
-                        exec
-                    ),
-                    none: {},
-                },
-            },
-            
-            range: from?.to.(
-                from >= to => {none};
-                val = from;
-                from? = from + 1;
-                {some, val}
-            ),
-
-            /* this main closure will be called
-              when we execute the program */
-            main: args.(
-                total? = 0;
-                for (range 10 20) val.(
-                    total? = total + val;
-                    print total
-                ) exec
-            ),"
-        ))
-        .is_ok());
-    }
-}
+mod lexer;
+mod offset;
+pub mod packrat;
+mod parser;
