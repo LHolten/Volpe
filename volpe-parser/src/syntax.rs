6a79f1eed4eeb918f92b6e595f3bef0204b81e71
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-03-23 15:55:47 +0100

    Add comments to Display implementation

--- volpe-parser/src/syntax.rs
+++ volpe-parser/src/syntax.rs
@@ -16,28 +16,37 @@ const INDENT: &'static str = "  ";
 impl fmt::Display for Lexeme {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut indent = 0;
+        // we treat next_lexemes as a stack of remembered lexemes
         let mut next_lexemes = vec![self];
 
         while let Some(lexeme) = next_lexemes.pop() {
+            // for each rule we check if it is valid by looking at length
+            // if valid, we write the rule and remember the next lexeme
+            // once we finish a lexeme chain we use these remembered lexemes to continue
             for (i, rule) in lexeme.rules.iter().enumerate() {
                 if rule.length > Offset::default() {
+                    // write the rule
                     for _ in 0..indent {
                         f.write_str(INDENT)?;
                     }
                     write!(f, "{:?}\n", RuleKind::from(i))?;
+                    // remember the next lexeme for later
                     if let Some(next) = &rule.next {
                         next_lexemes.push(next)
                     }
                     indent += 1;
                 }
             }
+            // write the lexeme
             for _ in 0..indent {
                 f.write_str(INDENT)?;
             }
             write!(f, "{:?}\n", lexeme.string)?;
+            // this is where we follow a lexeme chain
             if let Some(next) = &lexeme.next {
                 next_lexemes.push(next)
             } else {
+                // we reached the end of a chain, i.e. end of a rule
                 indent -= 1;
             }
         }
