643f25f01367f6ba9f6492482a620c958bfbef69
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-04-03 01:13:06 +0200

    Join io threads

--- volpe-ls/src/main.rs
+++ volpe-ls/src/main.rs
@@ -4,14 +4,15 @@ extern crate volpe_parser;
 
 mod capabilities;
 mod document;
-mod handler;
-mod lsp_utils;
+mod dispatch;
+mod handlers;
+// mod lsp_utils;
 mod semantic_tokens;
 mod server;
 use server::Server;
 
 fn main() {
-    let (connection, _io_threads) = Connection::stdio();
+    let (connection, io_threads) = Connection::stdio();
 
     let (initialize_id, initialize_params) = connection.initialize_start().unwrap();
     let _init_params: lsp_types::InitializeParams =
@@ -28,5 +29,16 @@ fn main() {
         .initialize_finish(initialize_id, initialize_result)
         .unwrap();
 
+    // TODO Add this back in when doing multithreading.
+    // https://github.com/rust-analyzer/rust-analyzer/blob/2c5f905c67bb8e3bfb19efa0de5ab911e63f8e42/crates/rust-analyzer/src/main_loop.rs#L32
+    // #[cfg(windows)]
+    // unsafe {
+    //     use winapi::um::processthreadsapi::{GetCurrentThread, SetThreadPriority};
+    //     let thread = GetCurrentThread();
+    //     let thread_priority_above_normal = 1;
+    //     SetThreadPriority(thread, thread_priority_above_normal);
+    // }
+    
     Server::new(connection).run();
+    io_threads.join().unwrap();
 }
