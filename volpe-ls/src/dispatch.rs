9762b406d2fa381f1ad1243c89c662e22a3b886d
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-04-03 01:12:19 +0200

    Rename handlers to dispatchers

--- volpe-ls/src/dispatch.rs
+++ volpe-ls/src/dispatch.rs
@@ -1,13 +1,14 @@
 use lsp_server::{Notification, Request};
 
 use crate::server::Server;
+use crate::handlers::RequestResult;
 
-pub struct NotificationHandler<'a> {
+pub struct NotificationDispatcher<'a> {
     pub notification: Option<Notification>,
     pub server: &'a mut Server,
 }
 
-impl<'a> NotificationHandler<'a> {
+impl<'a> NotificationDispatcher<'a> {
     pub fn on<N: lsp_types::notification::Notification>(
         &mut self,
         f: fn(&mut Server, N::Params),
@@ -28,25 +29,25 @@ impl<'a> NotificationHandler<'a> {
     }
 
     pub fn finish(&mut self) {
-        if let Some(notif) = &self.notification {
+        if let Some(notification) = &self.notification {
             // notification starting with $/ are optional
-            if !notif.method.starts_with("$/") {
+            if !notification.method.starts_with("$/") {
                 self.server
-                    .show_error_message(format!("unhandled notification: {:?}", notif));
+                    .show_error_message(format!("unhandled notification: {:?}", notification));
             }
         }
     }
 }
 
-pub struct RequestHandler<'a> {
+pub struct RequestDispatcher<'a> {
     pub request: Option<Request>,
     pub server: &'a mut Server,
 }
 
-impl<'a> RequestHandler<'a> {
+impl<'a> RequestDispatcher<'a> {
     pub fn on<R: lsp_types::request::Request>(
         &mut self,
-        f: fn(&mut Server, R::Params) -> R::Result,
+        f: fn(&mut Server, R::Params) -> RequestResult<R::Result>,
     ) -> &mut Self {
         let req = match self.request.take() {
             Some(it) => it,
@@ -59,18 +60,16 @@ impl<'a> RequestHandler<'a> {
                 return self;
             }
         };
+
         // TODO multithread this.
         let result = f(self.server, params);
-        // TODO Handle unsuccessful requests.
-        self.server
-            .send(lsp_server::Response::new_ok(id, &result).into());
+        self.server.send_response::<R>(id, result);
         self
     }
 
     pub fn finish(&mut self) {
         if let Some(req) = &self.request {
-            self.server
-                .show_error_message(format!("unknown request: {:?}", req));
+            self.server.show_error_message(format!("unknown request: {:?}", req));
             let response = lsp_server::Response::new_err(
                 req.id.clone(),
                 lsp_server::ErrorCode::MethodNotFound as i32,
