535cd1f57e1beb858b5c8548a4707a3d8053f517
Viliam Vadocz <viliam.vadocz@gmail.com>
2021-06-16 23:57:58 +0200

    Add failing test

--- volpe-parser-2/src/lib.rs
+++ volpe-parser-2/src/lib.rs
@@ -9,7 +9,7 @@ mod shunting;
 
 #[cfg(test)]
 mod test {
-    use crate::{file::File, offset::Offset};
+    use crate::{file::{File, PatchResult}, offset::Offset};
 
     #[test]
     fn parse() {
@@ -22,5 +22,14 @@ mod test {
         file.patch(Offset::char(0), Offset::char(0), "+ (1) 1".to_string())
             .unwrap();
         dbg!(file.rule());
+    }
+
+    #[test]
+    fn fuzz_incr_edits_001() -> PatchResult {
+        let mut file = File::default();
+        file.patch(Offset::default(), Offset::default(), "ȃ".to_string())?;
+        file.patch(Offset::char(1), Offset::char(0), "".to_string())?;
+        file.rule();
+        Ok(())
     }
 }
