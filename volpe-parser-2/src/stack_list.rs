585fb1181a74bdc0006c6ee5140e6dd6168fcd0d
Hytak <lhc.holten@gmail.com>
2021-08-18 16:00:06 +0200

    Add pop function to StackList

--- volpe-parser-2/src/stack_list.rs
+++ volpe-parser-2/src/stack_list.rs
@@ -1,19 +1,11 @@
-#[derive(Clone, Copy)]
-struct Inner<'b, T> {
-    head: &'b T,
-    tail: &'b StackList<'b, T>,
+struct Inner<'a, T> {
+    head: &'a T,
+    tail: &'a StackList<'a, T>,
 }
 
-#[derive(Clone, Copy)]
-pub struct StackList<'b, T>(Option<Inner<'b, T>>);
+pub struct StackList<'a, T>(Option<Inner<'a, T>>);
 
-impl<'b, T> Default for StackList<'b, T> {
-    fn default() -> Self {
-        Self(None)
-    }
-}
-
-impl<'b, T: PartialEq> StackList<'b, T> {
+impl<'a, T: PartialEq> StackList<'a, T> {
     pub fn find(&self, value: &T) -> Option<usize> {
         let inner = self.0.as_ref()?;
         if inner.head == value {
@@ -23,15 +15,36 @@ impl<'b, T: PartialEq> StackList<'b, T> {
         }
     }
 
-    pub fn push(&'b self, value: &'b T) -> Self {
+    pub fn push(&'a self, value: &'a T) -> Self {
         Self(Some(Inner {
             head: value,
             tail: self,
         }))
     }
 
-    // pub fn pop(&self) -> Option<&'b Self> {
-    //     let inner = self.0.as_ref()?;
-    //     Some(inner.tail)
-    // }
+    pub fn pop(&self) -> Option<Self> {
+        Some(*self.0.as_ref()?.tail)
+    }
+}
+
+impl<'a, T> Copy for Inner<'a, T> {}
+
+impl<'a, T> Clone for Inner<'a, T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<'a, T> Copy for StackList<'a, T> {}
+
+impl<'a, T> Clone for StackList<'a, T> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<'a, T> Default for StackList<'a, T> {
+    fn default() -> Self {
+        Self(None)
+    }
 }
